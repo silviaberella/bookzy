cf79ad726915c05426534ec011022b61bc1567ef
student <silvia.berella@web.de>
2022-03-12 22:42:12 +0000

    Update file

--- src/main/java/bookzy/com/example/bookzy/BookzyApplication.java
+++ src/main/java/bookzy/com/example/bookzy/BookzyApplication.java
@@ -5,15 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookzyApplication {
-	private final String message = "Welcome to my Bookzy Store";
-	//public App() {}
 	public static void main(String[] args) {
 		SpringApplication.run(BookzyApplication.class, args);
-		System.out.println(new App().getMessage());
 	}
-
-	private final String getMessage() {
-		return message;
-	}
-
 }
